27104dfa1fe02207296354dcb03bfeb2f2600f4e
Daniel <virusxd521@gmail.com>
2021-05-21 20:00:31 +0300

    new commit

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
 let arrow = document.querySelector(".arrow");
+let arrow_on_tablets = document.querySelector(".button_arrow");
+let width_of_page = window.innerWidth;
 
 document.body.onscroll = () => {    
-    if (scrollY >= 256) {
+    if (scrollY >= 80 && width_of_page <= 700) {
         arrow.style.display = "inline-block";    
-    } else {
+    } else{
         arrow.style.display = "none";    
     }
-    
-    
-
 }
